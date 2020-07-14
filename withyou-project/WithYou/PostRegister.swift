e5e234f04c7c8ef588e221b44d7bb6461e108a23
Unalian <Dengmengda@gmail.com>
2020-07-15 00:58:45 +0800

    login and register post(something wrong with url)

--- withyou-project/WithYou/PostRegister.swift
+++ withyou-project/WithYou/PostRegister.swift
@@ -10,23 +10,19 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 import SSZipArchive
-func zip()->URL
-{   let PostImagePathString :String  = PostImagePath!.path
-    let PostImagePathZipString:String = "/User/picture.zip"
-    SSZipArchive.createZipFile(atPath: PostImagePathZipString, withContentsOfDirectory:PostImagePathString)
-    let PostImagePathZip = NSURL.fileURL(withPath: PostImagePathZipString)
-    return PostImagePathZip
-}
+
 func PostRegister(completion: @escaping (_ code: Int, _ msg: String) -> (), name: String, email:String, password: String) {
     var code = 100
     var msg = ""
+    print(getSize(url: PostImagePath!))
     AF.upload(multipartFormData: { (MultipartFormData) in
-        MultipartFormData.append(zip(), withName: "file")
+        MultipartFormData.append(PostImagePath!, withName: "file")
         MultipartFormData.append(name.data(using: String.Encoding.utf8, allowLossyConversion: false)!, withName: "name")
         MultipartFormData.append(email.data(using: String.Encoding.utf8, allowLossyConversion: false)!, withName: "email")
         MultipartFormData.append(password.data(using: String.Encoding.utf8, allowLossyConversion: false)!, withName: "pass")
     }, to: "https://mbp.cetacis.dev/api/register")
     .responseJSON { (reponse) in
+        print(reponse.error)
         let json = JSON(reponse.data!)
         code = json["code"].intValue
         msg = json["msg"].string!
