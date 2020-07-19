9f8676e5d484482a0248b8bc753cf2f2e6abeb3a
kelolemon <cutesinapis@gmail.com>
2020-07-19 21:05:59 +0800

    add task match func

--- withyou-project/WithYou/MatchFunc.swift
+++ withyou-project/WithYou/MatchFunc.swift
@@ -18,6 +18,7 @@ func GetMatch() {
             // match success
             let friendemail = msg // friend email
             // add yourself task
+            User.TogetherTasks = [TogetherTask]()
             for task in Tasks[User.CurrentTaskId]{
                 User.TogetherTasks.append(TogetherTask(
                                             name: task,
@@ -70,6 +71,12 @@ func GetMatch() {
                     Partner = UserInfo
                 }
             }, email: friendemail)
+        } else {
+            if User.partner.count != 0 {
+                GetFriendInfo(completion: { (UserInfo) in
+                    Partner = UserInfo
+                }, email: User.partner)
+            }
         }
     }, email: User.email, taskid: User.CurrentTaskId)
 }
